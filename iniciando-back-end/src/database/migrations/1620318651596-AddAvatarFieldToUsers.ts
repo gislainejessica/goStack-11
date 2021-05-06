9ee956bbfca25d1a13b5c40f75abe3f9e2d943a3
gislainejessica <gislainehjessica@gmail.com>
2021-05-06 14:13:30 -0300

    upload using multer

---
+++ iniciando-back-end/src/database/migrations/1620318651596-AddAvatarFieldToUsers.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AddAvatarFieldToUsers1620318651596 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('users', new TableColumn({
+      name: 'avatar', type: 'varchar', isNullable: true
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar')
+  }
+
+}
--- iniciando-back-end/src/middlewares/ensureAuthenticated.ts
+++ iniciando-back-end/src/middlewares/ensureAuthenticated.ts
@@ -8,7 +8,7 @@ interface TokenPayload {
   sub: string
 }
 
-export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
+export default function ensureAuthenticated(request: any, response: Response, next: NextFunction): void {
   const authHeader = request.headers.authorization
 
   if (!authHeader) {
