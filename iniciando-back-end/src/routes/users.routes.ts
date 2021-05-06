a666051e47103852418eaa9530bf4d6b8043b488
gislainejessica <gislainehjessica@gmail.com>
2021-05-06 16:34:16 -0300

    updated image files

--- iniciando-back-end/src/routes/users.routes.ts
+++ iniciando-back-end/src/routes/users.routes.ts
@@ -6,6 +6,9 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import CreateUserService from '../services/CreateUserService'
 
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+
+
 const usersRouter = Router();
 const upload = multer(uploadConfig)
 
@@ -27,8 +30,22 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  response.json({ ok: true })
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request: any, response) => {
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService()
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    })
+
+    // @ts-ignore
+    delete user.password
+
+    return response.json(user)
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 })
 
 export default usersRouter;
--- iniciando-back-end/src/server.ts
+++ iniciando-back-end/src/server.ts
@@ -2,10 +2,13 @@ import 'reflect-metadata';
 import express, { json } from 'express';
 import './database';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 app.use(json());
+app.use('/files', express.static(uploadConfig.directory))
 app.use(routes);
+
 
 app.listen(3333, () => {
   console.log(' server started in port 3333');
